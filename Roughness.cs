88fd2eba2fa2d0cd1ac6460ee2cedc34dea38eb1
Sebastián Miranda <smiranda9910@gmail.com>
2024-09-18 17:43:43 +0200

    ficheros dist.gps y dist.pro

--- Roughness.cs
+++ Roughness.cs
@@ -1,4 +1,5 @@
-﻿using DocumentFormat.OpenXml.Drawing.Charts;
+﻿using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.EMMA;
 using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml.Wordprocessing;
@@ -12,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace LCMS_ODO_GPS_GENERATOR
 {
@@ -24,7 +26,15 @@ namespace LCMS_ODO_GPS_GENERATOR
         public List<double> listaPromediosRight = new List<double>();
         public List<double> listaPromediosLeft = new List<double>();
         public List<Incidencia> listaIncidencias = new List<Incidencia>();
-        public string fecha;
+        public List<double> listaLatitud = new List<double>();
+        public List<double> listaLongitud = new List<double>();
+        public List<double> listaValoresErdLeft = new List<double>();
+        public List<double> listaValoresErdRight = new List<double>();
+
+        public double valorASumar;
+
+
+        public string fecha = "";
 
 
         public void recogerSubCarpetas(string ruta)
@@ -45,7 +55,8 @@ namespace LCMS_ODO_GPS_GENERATOR
                     //Le pasamos el nombre de la subcarpeta para que procese los ficheros que contiene
                     procesarArchivosSubcarpeta(subcarpeta, dir.FullName);
 
-                    limpiarListaYFecha();
+                    //Antes de empezar a escribir el siguiente fichero referente a la carpeta siguiente, reiniciamos datos
+                    limpiarListasYFecha();
                 }
             }
             catch (Exception ex)
@@ -54,10 +65,13 @@ namespace LCMS_ODO_GPS_GENERATOR
             }
         }
 
-        private void limpiarListaYFecha()
+        private void limpiarListasYFecha()
         {
             listaPromediosLeft.Clear();
             listaPromediosRight.Clear();
+            listaIncidencias.Clear();
+            listaLongitud.Clear();
+            listaLatitud.Clear();
             fecha = string.Empty;
         }
 
@@ -68,12 +82,170 @@ namespace LCMS_ODO_GPS_GENERATOR
 
             foreach (string archivoXML in archivosXML)
             {
-                leerRoughnessYFecha(archivoXML);
+                //Leemos roughness, fecha y eventos
+                leerDatosXml(archivoXML);
             }
 
             //Una vez hemos finalizado de leer todos los archivos de esa carpeta, generamos archivo.
             generarArchivoIRI(rutaDestinoCarpetaArchivos, nombreCarpeta);
             generarArchivoEvent(rutaDestinoCarpetaArchivos, nombreCarpeta);
+            generarArchivoGpsimp(rutaDestinoCarpetaArchivos, nombreCarpeta);
+
+
+            //Después de generar los archivos IRI, EVT y GPSIMP, procesaremos los archivos .erd
+            string[] archivosErd = Directory.GetFiles(rutaSubcarpeta, "*.erd", SearchOption.TopDirectoryOnly);
+
+            if (archivosErd.Length > 0)
+            {
+                foreach (string archivoErd in archivosErd)
+                {
+                    //hola/hg_L.erd
+                    bool esLeft;
+                    string ultimCaracter = archivoErd.Substring(archivoErd.Length - 5, 1);
+                    if (ultimCaracter == "L")
+                        esLeft = true;
+                    else //Es Right
+                        esLeft = false;
+
+                    leerDatosErd(archivoErd, esLeft);
+                }
+
+                generarArchivoPro(rutaDestinoCarpetaArchivos, nombreCarpeta);
+
+            }
+
+
+        }
+
+        private void generarArchivoPro(string rutaDestinoCarpetaArchivos, string nombreCarpeta)
+        {
+            try
+            {
+                string nomCarpMod = modificarNomCarp(nombreCarpeta);
+                string nombreArchivo = rutaDestinoCarpetaArchivos + "\\" + "LCMS" + fecha + nomCarpMod + ".dist.pro.txt";
+
+                using (StreamWriter escritor = new StreamWriter(nombreArchivo))
+                {
+                    //Headers:
+                    //Re - calibrationfile: 
+
+                    //Data:
+                    //Distance [m], Laser 1 [mm], Laser 2 [mm]
+                    escritor.WriteLine("Headers:");
+                    escritor.WriteLine("Re-calibrationfile:");
+                    escritor.WriteLine();
+                    escritor.WriteLine("Data:");
+                    escritor.WriteLine("Distance [m], Laser 1 [mm], Laser 2 [mm]");
+
+                    int cont = 0;
+                    
+                    while (listaValoresErdRight.Count > cont)
+                    {
+                        escritor.WriteLine(valorASumar.ToString().Replace(",", ".") + ", " + listaValoresErdRight[cont].ToString("F6").Replace(",", ".") + ", " + listaValoresErdLeft[cont].ToString("F6").Replace(",", "."));
+                        valorASumar += valorASumar;
+                        cont++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la función generarArchivoPro: " + ex.Message);
+            }
+        }
+
+        private void leerDatosErd(string archivoXML, bool esLeft)
+        {
+            bool despuesDeEND = false;
+            bool segundaFila = false;
+
+            // Leer todas las líneas del fichero
+            string[] lineas = File.ReadAllLines(archivoXML);
+
+            // Recorrer las líneas una por una
+            foreach (string linea in lineas)
+            {
+                string lineaLimpia = linea.Trim();
+
+                // Si encontramos "END", marcamos el inicio de los valores
+                if (lineaLimpia == "END")
+                {
+                    despuesDeEND = true;
+                    continue;
+                }
+
+                if (lineaLimpia.Contains("ERDFILEV2"))
+                {
+                    segundaFila = true;
+                    continue;
+                }
+
+                if (segundaFila)
+                {
+                    //xxx 
+                    //@SM 18/09/2024
+                    //Tratar el valorASumar, comentar con Irene 
+                    string[] datosSegundaFila = linea.Split(',');
+                    int longitud = datosSegundaFila.Length;
+                    valorASumar = Convert.ToDouble(datosSegundaFila[longitud - 3], CultureInfo.InvariantCulture);
+                }
+
+                // Si estamos después de "END", intentamos parsear los valores
+                if (despuesDeEND)
+                {
+                    double valorDou = Convert.ToDouble(linea, CultureInfo.InvariantCulture);
+                    if (esLeft)
+                        listaValoresErdLeft.Add(valorDou);
+                    else
+                        listaValoresErdRight.Add(valorDou);
+
+                }
+            }
+        }
+
+        private void generarArchivoGpsimp(string rutaDestinoCarpetaArchivos, string nombreCarpeta)
+        {
+            try
+            {
+                string nomCarpMod = modificarNomCarp(nombreCarpeta);
+                string nombreArchivo = rutaDestinoCarpetaArchivos + "\\" + "LCMS" + fecha + nomCarpMod + ".dist.gpsimp.txt";
+
+                using (StreamWriter escritor = new StreamWriter(nombreArchivo))
+                {
+                    //Headers:
+                    //Re-calibrationfile: 
+
+                    //Parameters:
+                    //Distance interval: 10.0000 m
+                    //Use time interval: no
+                    //Time interval[s]: 1.0000
+
+                    //Data:
+                    //Distance [m], Time [s], Latitude [dd.dddd], Longitude [dd.dddd], Height [m], Height of geoid [m], DOP, Satellites, Fixtype, GPStime, Method, Vcar [rad]
+                    escritor.WriteLine("Headers:");
+                    escritor.WriteLine("Re-calibrationfile:");
+                    escritor.WriteLine();
+                    escritor.WriteLine("Parameters:");
+                    escritor.WriteLine("Distance interval: 10.0000 m");
+                    escritor.WriteLine("Use time interval: no");
+                    escritor.WriteLine("Time interval [s]: 1.0000");
+                    escritor.WriteLine();
+                    escritor.WriteLine("Data:");
+                    escritor.WriteLine("Distance [m], Time [s], Latitude [dd.dddd], Longitude [dd.dddd], Height [m], Height of geoid [m], DOP, Satellites, Fixtype, GPStime, Method, Vcar [rad]");
+
+                    int cont = 0;
+                    double distancia = 0; //representada en metros, va de 10 en 10
+                    while (listaLongitud.Count > cont)
+                    {
+                        escritor.WriteLine(distancia + ", , " + listaLatitud[cont].ToString().Replace(",", ".") + ", " + listaLatitud[cont].ToString().Replace(",", ".") + ", , , , , , , ,");
+                        distancia += 10;
+                        cont++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la función generarArchivoPro: " + ex.Message);
+            }
         }
 
         private void generarArchivoEvent(string rutaDestinoCarpetaArchivos, string nombreCarpeta)
@@ -106,12 +278,20 @@ namespace LCMS_ODO_GPS_GENERATOR
                     escritor.WriteLine("Name, Type, Code, Index, Active, Distance[m], Distance toward[m], Distance after[m], Text, Lasers, Latitude, Longitude, Height, GeoHeight, DOP, Satellites");
 
                     int cont = 0;
-                    int PK = 1;
+                    int PK = 0;
                     while (listaIncidencias.Count > cont)
                     {
-                        escritor.WriteLine("Event, " + listaIncidencias[cont].incidencia + ", 0, 0, yes, " + listaIncidencias[cont].distanciaCab + ", , , " + "PK" + PK + ", , 0.0, 0.0, 0.0, 0.0, 0.0, 0");
+
+                        if (listaIncidencias[cont].incidencia == 9 || listaIncidencias[cont].incidencia == 1)
+                        {
+                            escritor.WriteLine("Event, " + listaIncidencias[cont].incidencia + ", 0, 0, yes, " + listaIncidencias[cont].distanciaCab + ", , , " + "PK" + PK + ", , 0.0, 0.0, 0.0, 0.0, 0.0, 0");
+                            PK++;
+                        }
+                        else
+                        {
+                            escritor.WriteLine("Event, " + listaIncidencias[cont].incidencia + ", 0, 0, yes, " + listaIncidencias[cont].distanciaCab + ", , , " + listaIncidencias[cont].incidencia + "-" + ", , 0.0, 0.0, 0.0, 0.0, 0.0, 0");
+                        }
                         cont++;
-                        PK++;
                     }
 
                 }
@@ -165,6 +345,7 @@ namespace LCMS_ODO_GPS_GENERATOR
                     escritor.WriteLine("Include vertical beam displacement: yes");
                     escritor.WriteLine("Include beam rotation: yes");
                     escritor.WriteLine("Result interval: 10.0000 m:");
+                    escritor.WriteLine("IRI 1 => Derecha - IRI 2 => Izquierda");
                     escritor.WriteLine();
                     escritor.WriteLine("Data:");
                     escritor.WriteLine("Distance [m], IRI (1) [m/km], IRI (2) [m/km]");
@@ -211,7 +392,9 @@ namespace LCMS_ODO_GPS_GENERATOR
             return nomCarpMod;
         }
 
-        public void leerRoughnessYFecha(string archivoXML)
+
+        //Leemos Roiughness, Fecha y Eventos
+        public void leerDatosXml(string archivoXML)
         {
             try
             {
@@ -220,19 +403,38 @@ namespace LCMS_ODO_GPS_GENERATOR
 
                 using (XmlReader reader = XmlReader.Create(archivoXML))
                 {
+                    double longitud = 0;
+                    double latitud = 0;
                     var leftLeido = false;
-                    string nombreArchivo = Path.GetFileName(archivoXML);
+                    string nombreArchivo = System.IO.Path.GetFileName(archivoXML);
                     Incidencia inc = new Incidencia();
 
                     // Leer el archivo XML secuencialmente
                     while (reader.Read())
                     {
-
-                        // Detectar el inicio del nodo <DateAndTime>
-                        if (reader.NodeType == XmlNodeType.Element && reader.Name == "DateAndTime")
+                        // Detectar el inicio del nodo <SectionPosition>
+                        if (reader.NodeType == XmlNodeType.Element && reader.Name == "SectionPosition")
                         {
-                            fecha = reader.ReadElementContentAsString();
-                            fecha = convertirFecha(fecha);
+                            // Leer internamente los nodos <Longitude> y <Latitude> dentro de <SectionPosition>
+                            while (reader.Read())
+                            {
+                                if (reader.NodeType == XmlNodeType.Element && reader.Name == "Longitude")
+                                {
+                                    longitud = Convert.ToDouble(reader.ReadElementContentAsString(), CultureInfo.InvariantCulture);
+                                    listaLongitud.Add(longitud);
+                                }
+                                else if (reader.NodeType == XmlNodeType.Element && reader.Name == "Latitude")
+                                {
+                                    latitud = Convert.ToDouble(reader.ReadElementContentAsString(), CultureInfo.InvariantCulture);
+                                    listaLatitud.Add(latitud);
+                                }
+
+                                // Salir del bucle interno si se alcanza el final de <SectionPosition>
+                                if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "SectionPosition")
+                                {
+                                    break;
+                                }
+                            }
                         }
 
                         // Detectar el inicio del nodo <UserEventInformation>
@@ -245,6 +447,13 @@ namespace LCMS_ODO_GPS_GENERATOR
                             inc.distanciaCab = dist * 10;
                             inc.incidencia = Convert.ToInt32(valorIncidencia);
                             listaIncidencias.Add(inc);
+                        }
+
+                        // Detectar el inicio del nodo <DateAndTime>
+                        if (reader.NodeType == XmlNodeType.Element && reader.Name == "DateAndTime")
+                        {
+                            fecha = reader.ReadElementContentAsString();
+                            fecha = convertirFecha(fecha);
                         }
 
                         // Detectar el inicio del nodo <RoughnessInformation>
@@ -264,6 +473,8 @@ namespace LCMS_ODO_GPS_GENERATOR
                                     listaPromediosLeft.Add(promedioDatosIri);
                                     leftLeido = true;
 
+
+
                                 }
                                 else if (reader.NodeType == XmlNodeType.Element && reader.Name == "IRI" && leftLeido)
                                 {
@@ -275,10 +486,10 @@ namespace LCMS_ODO_GPS_GENERATOR
                                 }
 
                                 // Salir del bucle interno si se alcanza el final de <Roughness>
-                                //if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "Roughness")
-                                //{
-                                //    break;
-                                //}
+                                if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "Roughness")
+                                {
+                                    break;
+                                }
                             }
 
                         }
